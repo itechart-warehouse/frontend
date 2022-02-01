fa4048a17fede3e789b6876fd610d20e1e3ff685
Larisa Antokhina <laranto1985@gmail.com>
2022-02-01 15:39:21 +0300

    US-1.1 Styled login form, added Formik and validation lib yup

--- src/components/forms/login-form/LoginForm.tsx
+++ src/components/forms/login-form/LoginForm.tsx
@@ -1,7 +1,68 @@
+import {useFormik} from 'formik';
+import {Button, TextField} from "@mui/material";
+import * as yup from 'yup';
+
+interface Values {
+    email: string,
+    password: string
+}
+
+const validationSchema = yup.object({
+    email: yup
+        .string()
+        .email('Enter a valid email')
+        .required('Email is required'),
+    password: yup
+        .string()
+        .min(6, 'Password should be of minimum 8 characters length')
+        .required('Password is required'),
+});
+
+
 function LoginForm() {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: (user: Values) => {
+            console.log(JSON.stringify({user}));
+        },
+    });
+
     return (
-        <h1>Login form is here</h1>
-    )
+        <form onSubmit={formik.handleSubmit}>
+            <TextField
+                fullWidth
+                id="email"
+                name="email"
+                label="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                sx={{mb:3}}
+            />
+            <TextField
+                fullWidth
+                id="password"
+                name="password"
+                label="Password"
+                type="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                helperText={formik.touched.password && formik.errors.password}
+                sx={{mb:3}}
+            />
+            <Button color="primary" variant="contained" fullWidth type="submit">
+                Submit
+            </Button>
+        </form>
+    );
+
 };
 
-export default LoginForm;
+export default LoginForm
--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,10 +1,20 @@
 import LoginForm from "../../components/forms/login-form/LoginForm";
+import {Grid, Typography} from "@mui/material";
 
 function LoginPage() {
     return (
         <>
-            <h1>Login Page</h1>
-            <LoginForm/>
+            <Grid container spacing={0}
+                  direction="column"
+                  alignItems="center"
+                  justifyContent="center"
+                  style={{minHeight: '100vh'}}>
+                <Grid item>
+                    <Typography variant="h2" component="div" gutterBottom color="primary" align="center">Log
+                        in</Typography>
+                    <LoginForm/>
+                </Grid>
+            </Grid>
         </>
     )
 };
