3fc8ad21aec1647f432795bddbce6b525d8af5cc
Alina Pimoshenko <pimoshenko63@gmail.com>
2022-02-02 14:53:40 +0300

    US-2.5 Added files with packages, added routind in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from "./pages/home_page/HomePage";
-import './App.css'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import "./App.css";
+import RecoverPasswordPage from "./pages/recover-password/RecoverPasswordPage";
 
 function App() {
-    return (
-        <Routes>
-            <Route path='/home' element={<HomePage/>}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/recover" element={<RecoverPasswordPage />} />
+    </Routes>
+  );
 }
 
 export default App;
