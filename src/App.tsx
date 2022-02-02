66d9519c3cfb10f3b885df7ae5cd631b03f898de
Alina Pimoshenko <pimoshenko63@gmail.com>
2022-02-02 15:01:22 +0300

    US-2.5 Comentedd unusage parts

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import "./App.css";
-import RecoverPasswordPage from "./pages/recover-password/RecoverPasswordPage";
+//import RecoverPasswordPage from "./pages/recover-password/RecoverPasswordPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/home" element={<HomePage />} />
-      <Route path="/recover" element={<RecoverPasswordPage />} />
+      {/*<Route path="/recover" element={<RecoverPasswordPage />} />*/}
     </Routes>
   );
 }
