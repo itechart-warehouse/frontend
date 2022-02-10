12eabc4cde54db08c4cd03653814240059f429a6
lain <anonvtrede@gmail.com>
2022-02-10 16:43:31 +0300

    US-2.2 Resized create company form

--- src/pages/company/CreateCompanyPage.tsx
+++ src/pages/company/CreateCompanyPage.tsx
@@ -1,24 +1,29 @@
-import  CreateCompanyForm  from '../../components/forms/company-form/CreateCompanyForm';
+import CreateCompanyForm from '../../components/forms/company-form/CreateCompanyForm';
 import Header from "../../components/header/Header";
 import React from "react";
-import {Grid, Typography, Container} from "@mui/material";
+import {Grid, Typography, Container, Box} from "@mui/material";
+
 const mainContainerStyle = {
-    pt:3,
+    pt: 3,
 };
+
 const CreateCompanyPage = () => {
     return (
-            <Container maxWidth="xl" sx={mainContainerStyle}>
-                <Grid container spacing={0}
-                      direction="column"
-                      alignItems="center"
-                      justifyContent="center"
-                      style={{minHeight: '100vh'}}>
-                    <Grid item>
-                        <Typography variant="h2" component="div" gutterBottom color="primary" align="center">Create new Company</Typography>
-                        <CreateCompanyForm/>
-                    </Grid>
-                </Grid>
-            </Container>
+
+        <Grid container spacing={0}
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              style={{minHeight: '100vh'}}>
+            <Grid item>
+                <Typography variant="h2" component="div" gutterBottom color="primary" align="center">Create new
+                    Company</Typography>
+                <Box sx={{maxWidth: 500}}>
+                    <CreateCompanyForm/>
+                </Box>
+            </Grid>
+        </Grid>
+
     );
 };
 
