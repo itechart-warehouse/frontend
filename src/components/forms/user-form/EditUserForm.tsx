ca8433221aafa7d8729b83bbca513c920428d0ea
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2022-02-20 21:35:47 +0300

    US-2.10-Edit-user
    Select box working again

--- src/components/forms/user-form/EditUserForm.tsx
+++ src/components/forms/user-form/EditUserForm.tsx
@@ -95,7 +95,7 @@ function EditUserForm() {
     },
   });
   console.log("selected", user_role_id);
-
+  formik.values.userRoleId= user_role_id;
   return (
     <form onSubmit={formik.handleSubmit}>
       <TextField
@@ -125,7 +125,7 @@ function EditUserForm() {
         <Select
           labelId="role"
           id="role"
-          value={formik.values.userRoleId}
+          value={user_role_id}
           label="Role"
           onChange={handleChange}
           sx={{ mb: 3 }}
