2920e2e9e1fb4d668593b1f9fd6bf8e20416c86a
Lonely-fr <70891250+Lonely-fr@users.noreply.github.com>
2022-02-24 00:38:03 +0300

    Fixexd

--- src/services/clientApi.ts
+++ src/services/clientApi.ts
@@ -107,6 +107,7 @@ function initClientApi() {
               address: companyCredentials.companyAddress,
               phone: companyCredentials.companyPhone,
               email: companyCredentials.companyEmail,
+              active: companyCredentials.active,
             },
           },
           { headers: { authorization: jwt } }
