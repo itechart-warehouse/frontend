b776563d38347af358a260647587db8910420ba7
Alina Pimoshenko <pimoshenko63@gmail.com>
2022-04-05 17:13:23 +0300

    US-4 Added user info using Redux

--- src/store/loginSlice.ts
+++ src/store/loginSlice.ts
@@ -1,34 +1,53 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 export interface LoginState {
-    user: {
-        isLoggedIn: boolean,
-        jwt: string
-    }
+  user: {
+    isLoggedIn: boolean;
+    jwt: string;
+    email: string;
+  };
+  userCompany: {
+    name: string;
+  };
+  userWarehouse: {
+    name: string;
+  };
 }
 
 const initialState: LoginState = {
-    user: {
-        isLoggedIn: false,
-        jwt: ''
-    }
-
-}
+  user: {
+    isLoggedIn: false,
+    jwt: "",
+    email: "",
+  },
+  userCompany: {
+    name: "",
+  },
+  userWarehouse: {
+    name: "",
+  },
+};
 
 const loginSlice = createSlice({
-    name: 'user login logout',
-    initialState,
-    reducers: {
-        loginUser: (state, actions) => {
-            state.user.isLoggedIn = true
-            state.user.jwt = actions.payload
-        },
-        logoutUser: (state) => {
-            state.user.isLoggedIn = false
-            state.user.jwt = ''
-        },
-    }
-})
+  name: "user login logout",
+  initialState,
+  reducers: {
+    loginUser: (state, actions) => {
+      state.user.isLoggedIn = true;
+      state.user.jwt = actions.payload.headers.authorization;
+      state.user.email = actions.payload.data.resource.email;
+      state.userCompany.name = actions.payload.data.company.name;
+      state.userWarehouse.name = actions.payload.data.warehouse.name;
+    },
+    logoutUser: (state) => {
+      state.user.isLoggedIn = false;
+      state.user.jwt = "";
+      state.user.email = "";
+      state.userCompany.name = "";
+      state.userWarehouse.name = "";
+    },
+  },
+});
 
-export const {loginUser, logoutUser} = loginSlice.actions;
+export const { loginUser, logoutUser } = loginSlice.actions;
 export default loginSlice.reducer;
