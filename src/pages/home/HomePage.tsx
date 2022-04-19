8cfce37d09e507bb271ddb91898288a81026a173
Alina Pimoshenko <pimoshenko63@gmail.com>
2022-04-19 16:17:23 +0300

    US-4x Created and added components for home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,25 +1,47 @@
-import { Box, Grid, Typography } from "@mui/material";
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { Container, Grid } from "@mui/material";
+import WelcomeTitle from "../../components/main-page/WelcomeTitle";
+import HomeCalendar from "../../components/main-page/HomeCalendar";
+import HomeData from "../../components/main-page/HomeData";
 
-function HomePage() {
+export default function BasicGrid() {
   return (
-    <>
-      <Box>
+    <Container maxWidth="xl">
+      <Box sx={{ flexGrow: 1 }}>
         <Grid
           container
-          spacing={0}
-          direction="column"
+          spacing={10}
           alignItems="center"
           justifyContent="center"
           style={{ minHeight: "100vh" }}
+          paddingTop={"100px"}
         >
-          <Grid item xs={3}>
-            <Typography variant="h2">Welcome to your WarehouseAPP</Typography>
+          <Grid item xs={8} style={{ minWidth: "50vw" }} textAlign={"center"}>
+            <WelcomeTitle />
+          </Grid>
+          <Grid item xs={4} textAlign={"left"}>
+            <Box
+              component="img"
+              sx={{
+                height: 500,
+                width: 500,
+                maxHeight: { xs: 500, md: 500 },
+                maxWidth: { xs: 500, md: 500 },
+              }}
+              alt="Image"
+              src="https://assets.website-files.com/5f8ad5900964f1286fb46d05/6058f250c077d21886e5400a_pic-2.svg"
+            />
+          </Grid>
+          <Grid item xs={2} textAlign={"right"}></Grid>
+          <Grid item xs={4} textAlign={"right"}>
+            <HomeData />
+          </Grid>
+          <Grid item xs={5} alignItems={"center"}>
+            <HomeCalendar />
           </Grid>
         </Grid>
       </Box>
-    </>
+    </Container>
   );
 }
-
-export default HomePage;
