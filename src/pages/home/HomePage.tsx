1d301f025855bb7ab5fe8b98566d779652f0e480
Larisa Antokhina <laranto1985@gmail.com>
2022-04-20 09:19:50 +0300

    Fixed some styles

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -8,40 +8,38 @@ import HomeData from "../../components/main-page/HomeData";
 export default function BasicGrid() {
   return (
     <Container maxWidth="xl">
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={10}
-          alignItems="center"
-          justifyContent="center"
-          style={{ minHeight: "100vh" }}
-          paddingTop={"100px"}
-        >
-          <Grid item xs={8} style={{ minWidth: "50vw" }} textAlign={"center"}>
-            <WelcomeTitle />
-          </Grid>
-          <Grid item xs={4} textAlign={"left"}>
-            <Box
-              component="img"
-              sx={{
-                height: 500,
-                width: 500,
-                maxHeight: { xs: 500, md: 500 },
-                maxWidth: { xs: 500, md: 500 },
-              }}
-              alt="Image"
-              src="https://assets.website-files.com/5f8ad5900964f1286fb46d05/6058f250c077d21886e5400a_pic-2.svg"
-            />
-          </Grid>
-          <Grid item xs={2} textAlign={"right"}></Grid>
-          <Grid item xs={4} textAlign={"right"}>
-            <HomeData />
-          </Grid>
-          <Grid item xs={5} alignItems={"center"}>
-            <HomeCalendar />
-          </Grid>
+      <Grid
+        container
+        spacing={10}
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+        paddingTop={"100px"}
+      >
+        <Grid item xs={8} style={{ minWidth: "50vw" }} textAlign={"center"}>
+          <WelcomeTitle />
         </Grid>
-      </Box>
+        <Grid item xs={4} textAlign={"left"}>
+          <Box
+            component="img"
+            sx={{
+              height: "100%",
+              width: "100%",
+              maxHeight: { xs: 500, md: 500 },
+              maxWidth: { xs: 500, md: 500 },
+            }}
+            alt="Home image"
+            src="https://assets.website-files.com/5f8ad5900964f1286fb46d05/6058f250c077d21886e5400a_pic-2.svg"
+          />
+        </Grid>
+        <Grid item xs={2} textAlign={"right"}></Grid>
+        <Grid item xs={4} textAlign={"right"}>
+          <HomeData />
+        </Grid>
+        <Grid item xs={5} alignItems={"center"}>
+          <HomeCalendar />
+        </Grid>
+      </Grid>
     </Container>
   );
 }
