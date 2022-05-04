9fd4454136b3443ffe7e7f7bf7208e49ac67bcd1
Alina Pimoshenko <pimoshenko63@gmail.com>
2022-05-04 09:04:38 +0300

    MUI imports were added in one

--- src/components/main-page/HomeData.tsx
+++ src/components/main-page/HomeData.tsx
@@ -1,18 +1,21 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableContainer,
+  TableHead,
+  Paper,
+  Box,
+  TableCell,
+  tableCellClasses,
+} from "@mui/material";
 import { useEffect, useState } from "react";
 import { clientApi } from "../../services/clientApi";
 import { clearError, setError } from "../../store/errorSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
-import { Box } from "@mui/material";
 
 interface Consignments {
   id: number;
